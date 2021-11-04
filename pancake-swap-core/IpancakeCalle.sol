997cb428868197d1e91c59a63ab5588a55cf312f
Flash Loaner Expert <93416001+Orlandis19@users.noreply.github.com>
2021-11-04 04:58:14 -0400

    Delete IpancakeCalle.sol

--- pancake-swap-core/IpancakeCalle.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity >=0.6.12;
-
-SPDX-License-Identifier: UNLICENSED
-
-interface IPancakeCallee {
-    function pancakeCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
-}
